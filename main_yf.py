1a2f58151ad6c922ae1dcd88b6277d65bf54f7c1
pinetreeps <pangpangsongsong@sina.com>
2018-12-26 15:28:33 +0800

    debug

--- main_yf.py
+++ main_yf.py
@@ -139,5 +139,5 @@ def area_overview():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8290, debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8290, debug=True)
+    # app.run(debug=True)
